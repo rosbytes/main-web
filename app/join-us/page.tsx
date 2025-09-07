99fec48ed4db3a5b3d95b3909de53867ba8b0441
abhishek sajwan <abhisheksajwan2004@gmail.com>
2025-09-07 18:45:53 +0530

    completed joinUs page also fixed the width of Form in Contact Us section

--- app/join-us/page.tsx
+++ app/join-us/page.tsx
@@ -1,3 +1,52 @@
-export default function page() {
-  return <div>Join US page</div>;
+"use client";
+
+import Image from "next/image";
+import JoinCard from "@components/JoinCard";
+import { CardData } from "../../components/JoinPageData";
+
+type CardProps = {
+  title: string;
+  role: string;
+  description: string;
+  button: string;
+  gradient: string;
+  slogan: string;
+};
+
+export default function Page() {
+  return (
+    <div className="relative flex min-h-screen items-center justify-center overflow-x-hidden">
+      {/* left vector */}
+      <Image
+        src="/JoinUs/leftVector.svg"
+        alt="left vector"
+        width={320}
+        height={820}
+        className="absolute top-0 bottom-0 left-0 z-0 my-auto h-[90%] w-auto"
+      />
+
+      <div className="relative z-10 flex w-[50%] max-w-6xl flex-col items-center">
+        <h1 className="mb-2 text-center text-5xl font-black text-black uppercase">
+          JOIN THE REPUBLIC
+        </h1>
+        <h2 className="mb-7 px-6 py-2 text-center text-[1.15rem] font-semibold text-black">
+          Join R.O.S. as a <span className="font-bold">Vendor</span> or an{" "}
+          <span className="font-bold">Investor</span>.
+        </h2>
+        <div className="grid w-full max-w-3xl grid-cols-1 gap-10 md:grid-cols-2">
+          {(CardData as CardProps[]).map((card, i) => (
+            <JoinCard key={i} {...card} />
+          ))}
+        </div>
+      </div>
+      {/*r vector */}
+      <Image
+        src="/JoinUs/rightVector.svg"
+        alt="right vector"
+        width={320}
+        height={820}
+        className="absolute top-0 right-0 bottom-0 z-0 my-auto h-[90%] w-auto"
+      />
+    </div>
+  );
 }
